4bf2d2e5b94c7b0cfc9194fc4dda7d3b7c3049e4
computer <hal@computers-iMac.local>
2014-05-14 12:37:16 -0400

    Initial Commit

---
+++ StudyBuddy3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StudyBuddy3
+//
+//  Created by computer on 5/14/14.
+//  Copyright (c) 2014 computer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
